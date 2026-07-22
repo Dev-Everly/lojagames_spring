2c1419f76c8c8fc1b7c53a27d258f0fbb3c7d900
Dev-Everly <everlysantos98@gmail.com>
2026-07-22 14:37:44 -0300

    feat: adiciona model Categoria e cria relacionamento

--- lojagames/src/main/java/com/generation/lojagames/model/Produto.java
+++ lojagames/src/main/java/com/generation/lojagames/model/Produto.java
@@ -4,11 +4,14 @@ import java.math.BigDecimal;
 
 import org.hibernate.validator.constraints.URL;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -43,6 +46,11 @@ public class Produto {
 	@Column(length = 500)
 	private String foto;
 
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+	
 	public Long getId() {
 		return id;
 	}
